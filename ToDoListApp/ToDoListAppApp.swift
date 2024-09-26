f1e24d3353eea8b62e1729d914c0ca252ae54bad
Mason <mason.tyler.wooldridge@gmail.com>
2024-09-25 22:36:05 -0400

    Initial Commit

---
+++ ToDoListApp/ToDoListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListAppApp.swift
+//  ToDoListApp
+//
+//  Created by Mason Wooldridge on 9/25/24.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ToDoListAppTests/ToDoListAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListAppTests.swift
+//  ToDoListAppTests
+//
+//  Created by Mason Wooldridge on 9/25/24.
+//
+
+import Testing
+@testable import ToDoListApp
+
+struct ToDoListAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
