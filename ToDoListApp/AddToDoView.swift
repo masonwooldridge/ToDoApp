eb2de2e854df7d229bb26345aa45a283e4428bd9
Mason <mason.tyler.wooldridge@gmail.com>
2024-10-14 18:08:33 -0400

    Merge branch 'main' of https://github.com/masonwooldridge/ToDoApp

---
+++ ToDoListApp/AddToDoView.swift
@@ -0,0 +1,18 @@
+//
+//  AddToDoView.swift
+//  ToDoListApp
+//
+//  Created by Mason Wooldridge on 9/25/24.
+//
+
+import SwiftUI
+
+struct AddToDoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddToDoView()
+}
